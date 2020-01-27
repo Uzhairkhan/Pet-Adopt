1c50078ceb74124224cd76f216569a5e7ad1c19e
Uzhairkhan <uzhair_khan@outlook.com>
2020-01-27 18:03:40 +0530

    Pet Add

--- client/src/components/pets/PetForm.js
+++ client/src/components/pets/PetForm.js
@@ -10,10 +10,59 @@ class Form extends React.Component {
     return (
       <div className="border shadow rounded col-md-8 offset-md-2 ">
         <form>
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" className="form-control" />
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label for="name">Name</label>
+              <input type="text" className="form-control" id="name" />
+            </div>
+            <div className="form-group col-md-6">
+              <label for="age">Age</label>
+              <input type="text" className="form-control" id="age" />
+            </div>
           </div>
+
+          <div className="form-group">
+            <label for="inputAddress">Address</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputAddress"
+              placeholder="1234 Main St"
+            />
+          </div>
+          <div className="form-group">
+            <label for="inputAddress2">Address 2</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputAddress2"
+              placeholder="Apartment, studio, or floor"
+            />
+          </div>
+          <div className="form-row">
+            <div className="form-group col-md-4">
+              <label for="animal_type">Category</label>
+              <select id="animal_type" className="form-control">
+                <option selected>Choose...</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div className="form-group col-md-4">
+              <label for="breed">Breed</label>
+              <select id="breed" className="form-control">
+                <option selected>Choose...</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div className="form-group col-md-4">
+              <label for="inputZip">Gender</label>
+              <input type="text" className="form-control" id="inputZip" />
+            </div>
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Sign in
+          </button>
         </form>
       </div>
     );
